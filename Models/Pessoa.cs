b0e1aa8a915224462ff516075b1b9053af88a120
Paulo Marinho <paulomarinho.a.b@gmail.com>
2024-08-26 18:53:01 -0300

    commit antes do envio para o repo-remoto

---
+++ Models/Pessoa.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace novo_projeto.Models
+{
+    public class Pessoa
+    {
+        
+    }
+}
